a4ab343aca2957a0c4b6251f0eacb1e25fd0e9f9
Simon Arlott <simon.arlott.org.uk>
2010-07-17 23:51:45 +0100

    disable verbose mode if forking

--- pulsemon.h
+++ pulsemon.h
@@ -11,6 +11,10 @@
 #define xerror(msg) do { perror(msg); exit(EXIT_FAILURE); } while(0)
 #define cerror(msg, expr) do { if (expr) xerror(msg); } while(0)
 
+#ifdef FORK
+# undef VERBOSE
+#endif
+
 #ifdef VERBOSE
 # define _printf(...) printf(__VA_ARGS__)
 #else
