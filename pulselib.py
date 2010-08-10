d9c6003ef72ee7df20955f97084b1535398fe433
Simon Arlott <simon.arlott.org.uk>
2010-08-10 22:04:39 +0100

    split up python code into common handler library

---
+++ pulselib.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+# coding=utf8
+
+from __future__ import division
+from __future__ import print_function
+import datetime
+import pg
+import pgdb
+import select
+import sys
+import syslog
+import time
+
+class Pulses:
+	class NoSuchMeter(Exception):
+		pass
+
+	def __init__(self, db, meter):
+		data = db.select1("SELECT id FROM meters WHERE id = %(id)s", { "id": meter })
+		if data is None:
+			raise self.NoSuchMeter(meter)
+
+		self.meter = meter
+		self.db = db
+
+	def tso(self, ts):
+		return datetime.datetime.strptime(ts, '%Y-%m-%d %H:%M:%S.%f+00')
+
+	def tsd(self, cur, prev):
+		delta = self.tso(cur) - self.tso(prev)
+		if sys.version_info < (2, 7):
+			return (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6
+		else:
+			return delta.total_seconds()
+
+	def get_reading(self):
+		TS, VALUE = range(0, 2)
+		CUR, PREV = range(0, 2)
+
+		print("Getting reading...")
+		data = self.db.select("SELECT ts,value FROM abs_pulses WHERE meter = %(id)s ORDER BY ts DESC LIMIT 2", { "id": self.meter })
+
+		reading = None
+		if len(data) == 2 and data[PREV][VALUE] is not None and data[CUR][VALUE] is not None:
+			reading = {
+				"ts": data[CUR][TS],
+				"value": data[CUR][VALUE],
+				"delta": self.tsd(data[CUR][TS], data[PREV][TS]),
+				"step": data[CUR][VALUE] - data[PREV][VALUE]
+			}
+
+		print("Reading: {0}".format(reading))
+		return reading
+
+class DB:
+	class Reconnect(Exception):
+		pass
+
+	def __init__(self):
+		self.db = None
+
+	def abort(self, e=None):
+		if e is not None:
+			print(e, file=sys.stderr)
+		if self.db is not None:
+			try:
+				self.db.close()
+			except pg.DatabaseError:
+				pass
+		self.db = None
+
+	def connect(self):
+		try:
+			if self.db is None:
+				print("Connecting to DB...")
+				self.db = pgdb.connect()
+				self.listen()
+				self.commit()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			return False
+		except self.Reconnect:
+			return False
+		else:
+			return True
+
+	def listen(self):
+		try:
+			c = self.db.cursor()
+			c.execute("SET TIME ZONE 0")
+			c.execute("LISTEN changed")
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def select(self, query, data):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			c = self.db.cursor()
+			c.execute(query, data)
+			data = c.fetchall()
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+		else:
+			return data
+
+	def select1(self, query, data):
+		data = self.select(query, data)
+		return data[0] if len(data) > 0 else None
+
+	def update(self, query, data):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			c = self.db.cursor()
+			c.execute(query, data)
+			rows = c.rowcount
+			c.close()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+		else:
+			return rows
+
+	def commit(self):
+		if self.db is None:
+			raise self.Reconnect
+		try:
+			self.db.commit()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def rollback(self):
+		if self.db is None:
+			raise self.Reconnect
+		try:
+			self.db.rollback()
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+	def wait(self):
+		if not self.connect():
+			raise self.Reconnect
+		try:
+			notify = self.db._cnx.getnotify()
+			if notify is None:
+				print("Listening...")
+
+			while notify is None:
+				if self.db._cnx.fileno() < 0:
+					raise self.Reconnect
+				select.select([self.db._cnx], [], [self.db._cnx])
+				notify = self.db._cnx.getnotify()
+			print("Notified")
+		except self.Reconnect:
+			self.abort()
+			raise self.Reconnect
+		except pg.DatabaseError, e:
+			self.abort(e)
+			raise self.Reconnect
+
+class Log:
+	def __init__(self, name):
+		syslog.openlog(name)
+
+	def __call__(self, message):
+		syslog.syslog(message)
+
+class Handler:
+	def __init__(self, db, meter):
+		self.db = db
+		self.pulses = Pulses(db, meter)
+
+	def startup_delay(self):
+		pass
+
+	def is_newer_update(self, ts):
+		return False
+
+	def handle_pulse(self, ts, value, rate):
+		pass
+
+	def process_reading(self):
+		reading = self.pulses.get_reading()
+		if reading is not None and self.is_newer_update(reading["ts"]):
+			(ts, value, rate) = (reading["ts"], reading["value"], float(reading["step"]) / reading["delta"] * 3600)
+			print("[{0}] {1:09.3f} m³ ({2:04.2f} m³/hr)".format(ts, value, rate))
+
+			ok = self.handle_pulse(ts, value, rate)
+			try:
+				if ok:
+					self.db.commit()
+				else:
+					self.db.rollback()
+			except DB.Reconnect:
+				ok = False
+
+			return ok
+		else:
+			self.db.commit()
+			return None
+
+	def pulse_delay(self, ok):
+		pass
+
+	def wait_for_change(self):
+		self.db.wait()
+
+	def main_loop(self):
+		while True:
+			try:
+				ok = self.process_reading()
+				self.pulse_delay(ok)
+				if ok != False:
+					self.wait_for_change()
+				# allow some time for invalid readings to be reverted
+				time.sleep(2)
+			except DB.Reconnect:
+				while not self.db.connect():
+					time.sleep(5)
--- pulsetweet.py
+++ pulsetweet.py
@@ -1,181 +1,15 @@
 #!/usr/bin/env python
 # coding=utf8
 
-from __future__ import division
 from __future__ import print_function
 import argparse
-import datetime
 import daemon
 import json
 import oauth2 as oauth
-import pg
-import pgdb
-import select
+import pulselib
 import sys
-import syslog
 import time
 import urllib
-
-class Pulses:
-	class NoSuchMeter(Exception):
-		pass
-
-	def __init__(self, db, meter):
-		data = db.select1("SELECT id FROM meters WHERE id = %(id)s", { "id": meter })
-		if data is None:
-			raise self.NoSuchMeter(meter)
-
-		self.meter = meter
-		self.db = db
-
-	def tso(self, ts):
-		return datetime.datetime.strptime(ts, '%Y-%m-%d %H:%M:%S.%f+00')
-
-	def tsd(self, cur, prev):
-		delta = self.tso(cur) - self.tso(prev)
-		if sys.version_info < (2, 7):
-			return (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) * 10**6) / 10**6
-		else:
-			return delta.total_seconds()
-
-	def get_reading(self):
-		TS, VALUE = range(0, 2)
-		CUR, PREV = range(0, 2)
-
-		print("Getting reading...")
-		data = self.db.select("SELECT ts,value FROM abs_pulses WHERE meter = %(id)s ORDER BY ts DESC LIMIT 2", { "id": self.meter })
-
-		reading = None
-		if len(data) == 2 and data[PREV][VALUE] is not None and data[CUR][VALUE] is not None:
-			reading = {
-				"ts": data[CUR][TS],
-				"value": data[CUR][VALUE],
-				"delta": self.tsd(data[CUR][TS], data[PREV][TS]),
-				"step": data[CUR][VALUE] - data[PREV][VALUE]
-			}
-
-		print("Reading: {0}".format(reading))
-		return reading
-
-class DB:
-	class Reconnect(Exception):
-		pass
-
-	def __init__(self):
-		self.db = None
-
-	def abort(self, e=None):
-		if e is not None:
-			print(e, file=sys.stderr)
-		if self.db is not None:
-			try:
-				self.db.close()
-			except pg.DatabaseError:
-				pass
-		self.db = None
-
-	def connect(self):
-		try:
-			if self.db is None:
-				print("Connecting to DB...")
-				self.db = pgdb.connect()
-				self.listen()
-				self.commit()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			return False
-		except self.Reconnect:
-			return False
-		else:
-			return True
-
-	def listen(self):
-		try:
-			c = self.db.cursor()
-			c.execute("SET TIME ZONE 0")
-			c.execute("LISTEN changed")
-			c.close()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-
-	def select(self, query, data):
-		if not self.connect():
-			raise self.Reconnect
-		try:
-			c = self.db.cursor()
-			c.execute(query, data)
-			data = c.fetchall()
-			c.close()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-		else:
-			return data
-
-	def select1(self, query, data):
-		data = self.select(query, data)
-		return data[0] if len(data) > 0 else None
-
-	def update(self, query, data):
-		if not self.connect():
-			raise self.Reconnect
-		try:
-			c = self.db.cursor()
-			c.execute(query, data)
-			rows = c.rowcount
-			c.close()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-		else:
-			return rows
-
-	def commit(self):
-		if self.db is None:
-			raise self.Reconnect
-		try:
-			self.db.commit()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-
-	def rollback(self):
-		if self.db is None:
-			raise self.Reconnect
-		try:
-			self.db.rollback()
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-
-	def wait(self):
-		if not self.connect():
-			raise self.Reconnect
-		try:
-			notify = self.db._cnx.getnotify()
-			if notify is None:
-				print("Listening...")
-
-			while notify is None:
-				if self.db._cnx.fileno() < 0:
-					raise self.Reconnect
-				select.select([self.db._cnx], [], [self.db._cnx])
-				notify = self.db._cnx.getnotify()
-			print("Notified")
-		except self.Reconnect:
-			self.abort()
-			raise self.Reconnect
-		except pg.DatabaseError, e:
-			self.abort(e)
-			raise self.Reconnect
-
-class Log:
-	def __init__(self, twitter):
-		syslog.openlog("pulsetweet/{0}".format(twitter.account))
-
-	def __call__(self, message):
-		syslog.syslog(message)
 
 class Twitter:
 	class NoSuchAccount(Exception):
@@ -188,10 +22,10 @@ class Twitter:
 		if data is None:
 			raise self.NoSuchAccount(account)
 
+		self.db = db
 		self.account = account
 		self.client = oauth.Client(oauth.Consumer(data[CON_KEY], data[CON_SEC]), oauth.Token(data[ACC_TOK], data[ACC_SEC]))
-		self.log = Log(self)
-		self.db = db
+		self.log = pulselib.Log("pulsetweet/{0}".format(account))
 
 	def tweet(self, message, log=None):
 		ok = False
@@ -252,54 +86,24 @@ class Twitter:
 		print("Waiting {0}s".format(secs))
 		time.sleep(secs)
 
-class PulseTweeter:
+class PulseTweeter(pulselib.Handler):
 	def __init__(self, db, meter, account):
-		self.db = db
-		self.pulses = Pulses(db, meter)
+		pulselib.Handler.__init__(self, db, meter)
 		self.twitter = Twitter(db, account)
 
 	def startup_delay(self):
 		self.twitter.wait()
 
-	def process_reading(self):
-		reading = self.pulses.get_reading()
-		if reading is not None and self.twitter.is_newer_update(reading["ts"]):
-			tweet = "{0:09.3f} m³ ({1:04.2f} m³/hr)".format(reading["value"], float(reading["step"]) / reading["delta"] * 3600)
-			print("[{0}] {1}".format(reading["ts"], tweet))
-	
-			ok = self.twitter.tweet(tweet, "{0} <{1}>".format(tweet, reading["ts"]))
-			try:
-				if ok:
-					self.db.commit()
-				else:
-					self.db.rollback()
-			except DB.Reconnect:
-				ok = False
+	def is_newer_update(self, ts):
+		return self.twitter.is_newer_update(ts)
 
-			return ok
-		else:
-			self.db.commit()
-			return None
+	def handle_pulse(self, ts, value, rate):
+		tweet = "{0:09.3f} m³ ({1:04.2f} m³/hr)".format(value, rate)
+		return self.twitter.tweet(tweet, "{0} <{1}>".format(tweet, ts))
 
-	def tweet_delay(self, ok):
+	def pulse_delay(self, ok):
 		if ok is not None:
 			self.twitter.delay(ok)
-
-	def wait_for_change(self):
-		self.db.wait()
-
-	def main_loop(self):
-		while True:
-			try:
-				ok = self.process_reading()
-				self.tweet_delay(ok)
-				if ok != False:
-					self.wait_for_change()
-				# allow some time for invalid readings to be reverted
-				time.sleep(2)
-			except DB.Reconnect:
-				while not self.db.connect():
-					time.sleep(5)
 
 if __name__ == "__main__":
 	EXIT_SUCCESS, EXIT_FAILURE = range(0, 2)
@@ -310,7 +114,7 @@ if __name__ == "__main__":
 	parser.add_argument('account', help='Twitter account')
 	args = parser.parse_args()
 
-	db = DB()
+	db = pulselib.DB()
 	tweeter = PulseTweeter(db, args.meter, args.account)
 
 	if args.daemon:
